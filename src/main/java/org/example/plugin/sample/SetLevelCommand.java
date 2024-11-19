950e613989561fe9bbb50f150989219c26c87423
kouhei-urano <u06101217@gmail.com>
2024-11-19 13:39:11 +0900

    Merge remote-tracking branch 'github/Minecraft' into Minecraft

---
+++ src/main/java/org/example/plugin/sample/SetLevelCommand.java
@@ -0,0 +1,22 @@
+package org.example.plugin.sample;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+
+public class SetLevelCommand implements CommandExecutor {
+
+  @Override
+  public boolean onCommand (@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+    if (sender instanceof Player player)
+      if (args.length == 1) {
+        player.setLevel (Integer.parseInt (args[0]));
+      } else {
+        player.sendMessage ("No");
+      }
+    return false;
+  }
+}
